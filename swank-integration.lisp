b31773dc833ecaeade1609f781ab3c54a484501f
attila.lendvai <attila.lendvai@gmail.com>
2007-03-18 18:37:21 +0000

    Smarten up inspection, follow slime changes
    darcs-hash:20070318183721-6b9e8-b4ca1d0391232aec81988905a9a60d6e98fb3df9.gz

--- swank-integration.lisp
+++ swank-integration.lisp
@@ -29,14 +29,18 @@
     (funcall (name-of test))
     (swank::inspect-object *last-test-result* inspector)))
 
-(defun present-test-for-emacs (test inspector &key undefine-action actions-first)
+(defun present-test-for-emacs (test inspector &key name-only undefine-action actions-first)
   (when test
-    (bind ((actions `(,@(unless (lambda-list-of test)
-                         `((:action "[run]" ,(run-test-action-for-inspector test inspector))))
-                      ,@(when (and undefine-action
-                                   (find-test (name-of test) :otherwise nil))
-                         `(" " (:action "[undefine]" ,(lambda () (rem-test (name-of test))))))))
-           (value `((:value ,test))))
+    (bind ((actions `(,@(if (lambda-list-of test)
+                            (when actions-first
+                              `((:label "[run]")))
+                            `((:action "[run]" ,(run-test-action-for-inspector test inspector))))
+                      ,@ (when undefine-action
+                           (if (find-test (name-of test) :otherwise nil)
+                               (when actions-first
+                                 `((:label "[undefine]")))
+                               `((:action "[undefine]" ,(lambda () (rem-test (name-of test)))))))))
+           (value `((:value ,(if name-only (name-of test) test)))))
       (if actions-first
           (append actions '(" ") value)
           (append value '(" ") actions)))))
@@ -47,81 +51,104 @@
 (defun present-test-backtrace-for-emacs (description inspector)
   (iter (for context :in (test-context-backtrace-of description))
         (for idx :upfrom 0)
-        (collect (format nil "~D: " idx))
+        (when (first-time-p)
+          (appending `((:newline) (:label "Test backtrace:") (:newline))))
+        (collect (format nil "  ~D: " idx))
         (appending (present-context-for-emacs context inspector))
         (collect `(:newline))))
 
+(defun present-all-slots-for-emacs (object inspector)
+  (if *display-all-slots-in-inspector*
+      (append `((:newline)
+                (:action "[hide slots]" ,(lambda () (setf *display-all-slots-in-inspector* #f)))
+                (:newline))
+              (swank::all-slots-for-inspector object inspector))
+      `((:newline)
+        (:action "[show all slots]" ,(lambda () (setf *display-all-slots-in-inspector* #t))))))
+
+#-slime-wdim-branch
+(defun drop-labels (content)
+  (iter (for el :in content)
+        (if (and (consp el)
+                 (eq :label (first el)))
+            (collect (second el))
+            (collect el))))
+
+(defmacro inspector-result (title content)
+  #+slime-wdim-branch
+  `(list :title ,title :type nil :content ,content)
+  #-slime-wdim-branch
+  `(values ,title (drop-labels ,content)))
+
 (defmethod inspect-for-emacs ((global-context global-context) inspector)
-  (values "Stefil test results"
-          `("Executed tests: " (:value ,(hash-table-values (run-tests-of global-context))
-                                ,(princ-to-string (hash-table-count (run-tests-of global-context)))) (:newline)
-            "Executed assertions: " ,(princ-to-string (assertion-count-of global-context)) (:newline)
-            ,@(unless (emptyp (failure-descriptions-of global-context))
-                      `("Failures: " (:action "[rerun all failed tests]"
-                                      ,(lambda () (swank::inspect-object (run-failed-tests global-context) inspector)))))
-            (:newline)
-            ;; intentionally reverse the order by push'ing
-            ,@(iter (for description :in-vector (failure-descriptions-of global-context))
-                    (for context = (first (test-context-backtrace-of description)))
-                    (collect `(:action "[rerun]" ,(rerun-test-action-for-inspector context inspector)))
-                    (collect " ")
-                    (collect `(:value ,description))
-                    (collect `(:newline)))
-            ,@(when *display-all-slots-in-inspector*
-                    (swank::all-slots-for-inspector global-context inspector)))))
+  (inspector-result
+   "Stefil test results"
+   `((:label "Executed tests: ") (:value ,(hash-table-values (run-tests-of global-context))
+                                  ,(princ-to-string (hash-table-count (run-tests-of global-context)))) (:newline)
+     (:label "Executed assertions: ") ,(princ-to-string (assertion-count-of global-context))
+     (:newline) (:newline)
+     ,@(unless (emptyp (failure-descriptions-of global-context))
+               `((:label "List of failures: ")
+                 (:action "[rerun all failed tests]"
+                  ,(lambda () (swank::inspect-object (run-failed-tests global-context) inspector)))
+                 (:newline)))
+     ;; intentionally reverse the order by push'ing
+     ,@(iter (for description :in-vector (failure-descriptions-of global-context))
+             (for context = (first (test-context-backtrace-of description)))
+             (collect "  ")
+             (collect `(:action "[rerun]" ,(rerun-test-action-for-inspector context inspector)))
+             (collect " ")
+             (collect `(:value ,description))
+             (collect `(:newline)))
+     ,@(present-all-slots-for-emacs global-context inspector))))
 
 (defmethod inspect-for-emacs ((context context) inspector)
-  (values "Stefil test context"
-          `("Test: " (:value ,(test-of context)) " " (:action "[rerun]" ,(rerun-test-action-for-inspector context inspector)) (:newline)
-            "Test arguments: " (:value ,(test-arguments-of context)) (:newline)
-            "Real time spent in body: " (:value ,(real-time-spent-in-seconds context)) " sec" (:newline)
-            "Parent test frames:" (:newline)
-            ,@(iter (for parent-context :first (parent-context-of context) :then (parent-context-of parent-context))
-                    (while parent-context)
-                    (appending (reverse (present-context-for-emacs parent-context inspector)))
-                    (collect `(:newline)))
-            ,@(when *display-all-slots-in-inspector*
-                (swank::all-slots-for-inspector context inspector)))))
+  (inspector-result
+   "Stefil test context"
+   `((:label "Test:                    ") (:value ,(test-of context)) " " (:action "[rerun]" ,(rerun-test-action-for-inspector context inspector)) (:newline)
+     (:label "Test arguments:          ") ,@(awhen (test-arguments-of context) `((:value ,it))) (:newline)
+     (:label "Real time spent in body: ") (:value ,(real-time-spent-in-seconds context)) " sec"
+     (:newline) (:newline)
+     ,@(iter (for parent-context :first (parent-context-of context) :then (parent-context-of parent-context))
+             (while parent-context)
+             (when (first-time-p)
+               (collect `(:label "Parent test frames:"))
+               (collect `(:newline)))
+             (collect "  ")
+             (appending (reverse (present-context-for-emacs parent-context inspector)))
+             (collect `(:newline)))
+     ,@(present-all-slots-for-emacs context inspector))))
 
 (defmethod inspect-for-emacs ((failure failed-assertion) inspector)
-  (values "Failed Stefil assertion"
-          `("Form: " (:value ,(form-of failure)) (:newline)
-            "Test backtrace: " (:newline)
-            ,@(present-test-backtrace-for-emacs failure inspector)
-            ,@(when *display-all-slots-in-inspector*
-                (swank::all-slots-for-inspector failure inspector)))))
+  (inspector-result
+   "Failed Stefil assertion"
+   `((:label "Form: ") (:value ,(form-of failure)) (:newline)
+     ,@(present-test-backtrace-for-emacs failure inspector)
+     ,@(present-all-slots-for-emacs failure inspector))))
 
 (defmethod inspect-for-emacs ((description unexpected-error) inspector)
-  (values "Unexpected error in a Stefil test"
-          `("Condition: " (:value ,(condition-of description)) (:newline)
-            "Test backtrace: " (:newline)
-            ,@(present-test-backtrace-for-emacs description inspector)
-            ,@(when *display-all-slots-in-inspector*
-                (swank::all-slots-for-inspector description inspector)))))
+  (inspector-result
+   "Unexpected error in a Stefil test"
+   `("Condition: " (:value ,(condition-of description)) (:newline)
+     ,@(present-test-backtrace-for-emacs description inspector)
+     ,@(present-all-slots-for-emacs description inspector))))
 
 (defmethod inspect-for-emacs ((test test) inspector)
-  (values "Stefil test"
-          `("Name: " ,@(present-test-for-emacs test inspector :undefine-action #t) (:newline)
-            "Package: " (:value ,(package-of test)) (:newline)
-            "Compile before run?: " ,(if (compile-before-run-p test) "yes" "no") (:newline)
-            "Auto call by its suite?: " ,(if (auto-call-p test) "yes" "no") (:newline)
-            "Documentation: " ,@(when (documentation-of test) `((:value ,(documentation-of test)))) (:newline)
-            "Parent: " ,@(present-test-for-emacs (parent-of test) inspector) (:newline)
-            "Children: " (:newline)
-            ,@(iter (for (nil child) :in-hashtable (children-of test))
-                    (appending (present-test-for-emacs child inspector :actions-first #t))
-                    (collect `(:newline)))
-            ,@(when *display-all-slots-in-inspector*
-                (swank::all-slots-for-inspector test inspector)))))
-
-;; this will disable the header in the inspector, but vanilla Slime dies when NIL type is sent
-#+slime-wdim-branch
-(macrolet ((no-type-for (&rest args)
-             `(progn
-               ,@(iter (for type :in args)
-                       (collect `(defmethod swank::type-for-emacs ((thing ,type))
-                                  nil))))))
-  (no-type-for global-context context failed-assertion unexpected-error test))
+  (inspector-result
+   "Stefil test"
+   `((:label "Name:                    ") ,@(present-test-for-emacs test inspector :undefine-action #t :name-only #t) (:newline)
+     (:label "Package:                 ") (:value ,(package-of test)) (:newline)
+     (:label "Compile before run?:     ") ,(if (compile-before-run-p test) "yes" "no") (:newline)
+     (:label "Auto call by its suite?: ") ,(if (auto-call-p test) "yes" "no") (:newline)
+     (:label "Documentation:           ") ,@(when (documentation-of test) `((:value ,(documentation-of test)))) (:newline)
+     (:label "Parent:                  ") ,@(present-test-for-emacs (parent-of test) inspector) (:newline)
+     ,@(iter (for (nil child) :in-hashtable (children-of test))
+             (when (first-time-p)
+               (appending `((:newline) (:label "Children:") (:newline))))
+             (collect "  ")
+             (appending (present-test-for-emacs child inspector :actions-first #t))
+             (collect `(:newline)))
+     ,@(present-all-slots-for-emacs test inspector))))
 
 
 
