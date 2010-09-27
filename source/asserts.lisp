1bb6aaec6a86ba5b614b8541c32f87231d1f641f
Alexander Gavrilov <angavrilov@gmail.com>
2010-09-27 15:47:57 +0100

    Make with-captured-lexical-environment extensible.
    Ignore-this: 864122be940d141a8d1ea89d1d75465d
    
    Allow passing a function or macro name to be applied
    to the form in order to actually compile it. This is
    necessary for compile-file based tests in the walker.
    
    Also make the injector form readably printable.
    
    darcs-hash:20100927144757-38a44-705906c98377357426b932bfceb60ff97f9588eb.gz

--- source/asserts.lisp
+++ source/asserts.lisp
@@ -215,22 +215,26 @@
                            :format-control "FINISHES block did not finish: ~S"
                            :format-arguments ,whole))))))
 
-(defmacro with-captured-lexical-environment ((env-variable form) &body code)
+(defmacro %compile-quoted (form)
+  `(compile nil '(lambda () ,form)))
+
+(defmacro with-captured-lexical-environment ((env-variable form &key (compiler '%compile-quoted)) &body code)
   "Executes CODE with lexical environment captured at the point marked with the symbol -HERE-."
-  (with-unique-names (body injector-macro)
-    `(let ((,body (lambda (,env-variable) ,@code)))
+  ;; Use private interned symbols to ensure that the body can be printed readably:
+  (let ((body '.with-captured-lexical-environment/body.)
+        (injector-macro '.with-captured-lexical-environment/injector-macro.))
+    `(let ((,body (lambda (,env-variable)
+                    ;; TODO: wrap the body in our handlers that will prevent the errors/failed-asserts reaching COMPILE
+                    ,@code)))
        (declare (special ,body))        ; For the macrolet
        (handler-bind
            (#+sbcl(sb-ext:compiler-note #'muffle-warning)
             (warning #'muffle-warning))
-         (compile
-          nil
-          '(lambda ()
-            ,(subst `(macrolet ((,injector-macro (&environment env)
-                                  (declare (special ,body))
-                                  ;; wrap the body in our handlers that will prevent the errors/failed-asserts reaching COMPILE
-                                  (funcall ,body env)
-                                  (values)))
-                       (,injector-macro))
-                    '-here- form))))
+         (,compiler
+          ,(subst `(macrolet ((,injector-macro (&environment env)
+                                (declare (special ,body))
+                                (funcall ,body env)
+                                (values)))
+                     (,injector-macro))
+                  '-here- form)))
        (values))))
