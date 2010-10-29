2875cb30895afb6d4d6c92bf577fa25cb4c920f2
Kambiz Darabi <kambiz.darabi@gmail.com>
2010-10-29 17:24:14 +0100

    a tiny bit of documentation
    Ignore-this: 340a295990d232db02b5e3c81b9145ce
    
    darcs-hash:20101029162414-f5ba6-63516c4bd9d1ed59ccfae57e90e1a62698a92d20.gz

--- documentation/stefil.lisp
+++ documentation/stefil.lisp
@@ -20,4 +20,26 @@
       "Linux"))
   (chapter (:title "Tutorial")
     (paragraph ()
-      "TODO")))
+      "TODO"))
+  (chapter (:title "Tips and Tricks")
+    (paragraph (:title "Adding a new test interactively in slime")
+      "When working in slime, you might be interested in using
+slime-compile-defun \(usually bound to C-c C-c) on a new test, but you
+would be surprised: the test is not added to the suite (as you can
+verify by using slime-inspect on the name of the suite). Even if you
+have issued a (in-suite mysuite) before, the number of the tests in
+the suite remains constant.")
+    (paragraph ()
+      "In fact this behaviour is intentional (a 'feature', if you like):
+tests are not changing suites once they ended up in one of them so
+that you can C-c C-c freely \(without an in-package like support from
+slime). In other words: tests are sticking to suites. So, if you want
+to add a test to an existing suite, you have to use")
+    (shell-script ()
+      "(deftest (foo :in mysuite) () ... )"
+      "or"
+      "(def test (foo :in mysuite) () ...)")
+    (paragraph ()
+      "During the loading of the package with asdf, there is no need
+to specify the suite with the :in clause, so you might want to remove
+it afterwards.")))
