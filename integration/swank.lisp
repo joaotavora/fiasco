1f6dcde279bd83b227d38def1b67daccc09abdac
attila.lendvai <attila.lendvai@gmail.com>
2010-11-10 21:02:46 +0000

    label-value-line in slime must stay minimalistic, so kludge the situation locally.
    Ignore-this: ac49497c37add722a232bdecd267b20f
    
    darcs-hash:20101110210246-6b9e8-df6080f1e35a0ad32240049eebae8bede69c2dd0.gz

--- integration/swank.lisp
+++ integration/swank.lisp
@@ -4,6 +4,52 @@
 ;;;
 ;;; See LICENCE for details.
 
+(in-package :swank)
+
+;; The Slime maintainer is hostile to these, so we define this unconditionally for ourselves, so that
+;; it works on Slime HEAD, too.
+
+(defun hu.dwim.stefil::label-value-line (label value &key padding-length display-nil-value hide-when-nil
+                                         splice-as-ispec value-text (newline t))
+  "Create a control list which prints \"LABEL: VALUE\" in the inspector."
+  (if (or value (not hide-when-nil))
+      `((:label ,(princ-to-string label) ":")
+        ,@(when (or value display-nil-value)
+                (list " "))
+        ,@(when (and (or value display-nil-value)
+                     padding-length)
+                (list (make-array padding-length
+                                  :element-type 'character
+                                  :initial-element #\Space)))
+        ,@(when (or value display-nil-value)
+                (if splice-as-ispec
+                    (if (listp value) value (list value))
+                    `((:value ,value ,@(when value-text (list value-text))))))
+        ,@(if newline '((:newline)) nil))
+      (values)))
+
+(defmacro hu.dwim.stefil::label-value-line* (&rest label-values)
+  (let ((longest-label-length (loop
+                                 :for (label value) :in label-values
+                                 :maximize (if (stringp label)
+                                               (length label)
+                                               0))))
+  `(append ,@(loop
+                :for entry :in label-values
+                :if (and (consp entry)
+                         (not (consp (first entry)))
+                         (string= (first entry) '@))
+                  :appending (rest entry)
+                :else
+                  :collect (destructuring-bind
+                                 (label value &rest args &key &allow-other-keys) entry
+                             `(hu.dwim.stefil::label-value-line
+                               ,label ,value
+                               :padding-length ,(when (stringp label)
+                                                      (- longest-label-length
+                                                         (length label)))
+                               ,@args))))))
+
 (in-package :hu.dwim.stefil)
 
 (defun call-with-sldb-quit-restart (thunk restart)
@@ -80,7 +126,7 @@
 (defmethod swank-backend::emacs-inspect ((global-context global-context))
   (inspector-result
    "Stefil test results"
-   (swank-backend::label-value-line*
+   (label-value-line*
     ("Executed tests" (hash-table-values (run-tests-of global-context))
                       :value-text (princ-to-string (hash-table-count (run-tests-of global-context))))
     ("Executed assertions" (princ-to-string (assertion-count-of global-context)) :splice-as-ispec t)
@@ -103,7 +149,7 @@
 (defmethod swank-backend::emacs-inspect ((context context))
   (inspector-result
    "Stefil test context"
-   (swank-backend::label-value-line*
+   (label-value-line*
     ("Test" (test-of context))
     ("Test arguments" (test-arguments-of context) :display-nil-value nil)
     ("Real time spent in body" (let* ((time-spent (real-time-spent-in-seconds context)))
@@ -127,7 +173,7 @@
 (defmethod swank-backend::emacs-inspect ((failure failed-assertion))
   (inspector-result
    "Failed Stefil assertion"
-   (swank-backend::label-value-line*
+   (label-value-line*
     ("Form" (form-of failure))
     (@ (present-test-backtrace-for-emacs failure))
     (@ (present-all-slots-for-emacs failure)))))
@@ -135,7 +181,7 @@
 (defmethod swank-backend::emacs-inspect ((description unexpected-error))
   (inspector-result
    "Unexpected error in a Stefil test"
-   (swank-backend::label-value-line*
+   (label-value-line*
     ("Condition" (condition-of description))
     (@ (present-test-backtrace-for-emacs description))
     (@ (present-all-slots-for-emacs description)))))
@@ -143,7 +189,7 @@
 (defmethod swank-backend::emacs-inspect ((test test))
   (inspector-result
    "Stefil test"
-   (swank-backend::label-value-line*
+   (label-value-line*
     ("Name" (present-test-for-emacs test :undefine-action t :name-only t) :splice-as-ispec t)
     ("Package" (package-of test))
     ("Compile before run?" (if (compile-before-run-p test) "yes" "no") :splice-as-ispec t)
