70445a6e4aed54968194a706b8f097aaf5f89a7f
attila.lendvai <attila.lendvai@gmail.com>
2010-11-30 14:18:45 +0000

    added import-all-owned-symbols to deuplicates.lisp
    Ignore-this: 994b4dcead2fd166bb6b7d06de8c75dc
    
    darcs-hash:20101130141845-6b9e8-8e9fc6fa481174677368fae93f2666bda06270b4.gz

--- source/duplicates.lisp
+++ source/duplicates.lisp
@@ -161,3 +161,21 @@
   `(define-dynamic-context ,name ,direct-slots
      :defclass-macro-name ,defclass-macro-name
      ,@args))
+
+;; from hu.dwim.common
+(defun import-all-owned-symbols (source-package target-package &key (overwrite nil))
+  (setf source-package (find-package source-package))
+  (setf target-package (find-package target-package))
+  (let ((count 0))
+    (do-symbols (symbol source-package)
+      (let ((target-symbol-with-same-name (find-symbol (symbol-name symbol) target-package)))
+        (when (and (eq (symbol-package symbol) source-package)
+                   (or overwrite
+                       (not target-symbol-with-same-name)))
+          (when (and target-symbol-with-same-name
+                     (not (eq symbol target-symbol-with-same-name))
+                     overwrite)
+            (unintern target-symbol-with-same-name target-package))
+          (import symbol target-package)
+          (incf count))))
+    count))
