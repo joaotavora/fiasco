2ef568d41155d4e458ba987d92e2b3a4e690a6b4
attila.lendvai <attila.lendvai@gmail.com>
2010-11-30 14:19:29 +0000

    housekeeping in tests
    Ignore-this: 9d29f1df6b70fc1e1098218fb13bcba1
    
    darcs-hash:20101130141929-6b9e8-5a8c49ec118776bc7ebd280c72e37cb12195104c.gz

--- test/fixtures.lisp
+++ test/fixtures.lisp
@@ -10,7 +10,7 @@
 
 (defparameter *fixture-test-global* '())
 
-(defixture test-fixture
+(defixture test-fixture/unwind-protect
   (push 42 *fixture-test-global*)
   (unwind-protect
        (-body-)
@@ -18,25 +18,25 @@
 
 (defparameter *fixture-test-counter* 0)
 
-(defixture simple-test-fixture
+(defixture test-fixture/simple
   (incf *fixture-test-counter*)
   (-body-))
 
-(deftest fixtures ()
-  (with-fixture simple-test-fixture
+(deftest fixtures/simple ()
+  (with-fixture test-fixture/simple
     (is (not (zerop *fixture-test-counter*)))
-    (with-fixture test-fixture
+    (with-fixture test-fixture/unwind-protect
       (is (equal *fixture-test-global* '(42)))
-      (nested-fixtures1)
+      (%nested-fixtures1)
       (is (equal *fixture-test-global* '(42)))
-      (nested-fixtures2)
+      (%nested-fixtures2)
       (is (equal *fixture-test-global* '(42))))
     (is (equal *fixture-test-global* '()))))
 
-(defun nested-fixtures1 ()
-  (with-fixture test-fixture
+(defun %nested-fixtures1 ()
+  (with-fixture test-fixture/unwind-protect
     (is (equal *fixture-test-global* '(42)))))
 
-(deftest (nested-fixtures2 :auto-call nil) ()
-  (with-fixture test-fixture
+(deftest (%nested-fixtures2 :auto-call nil) ()
+  (with-fixture test-fixture/unwind-protect
     (is (equal *fixture-test-global* '(42)))))
