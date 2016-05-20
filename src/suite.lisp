ef81f3dcb613160ee1176db7fc40fccd4ce83530
Robert Smith <robert@rigetti.com>
2016-05-20 15:10:16 -0700

    document return values of #'RUN-PACKAGE-TESTS
    Fixes Issue #9

--- src/suite.lisp
+++ src/suite.lisp
@@ -86,8 +86,12 @@ PACKAGE-OPTIONS, automatically USEs the :FIASCO and :CL packages."
                                verbose
                                (stream *standard-output*)
                                interactive)
-  "Execute test suite associated with PACKAGE.
-PACKAGE defaults to the current package.
+  "Execute test suite associated with PACKAGE (default: current
+package). Returns two values:
+
+    1. A boolean indicating whether all tests were successful, and
+
+    2. A list of test run data structures.
 
 With optional INTERACTIVE, run tests interactively, i.e. break on
 errors and unexpected assertion failures.
