bb47d2fef4eb24cc16badc1c9a73d73c3a7e18f5
João Távora <joaotavora@gmail.com>
2020-05-14 17:10:55 +0100

    Fix #31: unbreak skipping tests on ACL
    * src/asserts.lisp (skip): Signal warning with SIGNAL, not WARN.

--- src/asserts.lisp
+++ src/asserts.lisp
@@ -218,7 +218,7 @@ there a superfulous quote at ~S?)" condition-type))
                            :format-arguments ,whole))))))
 
 (defun skip ()
-  (warn 'test-skipped))
+  (signal 'test-skipped))
 
 (defmacro skip-unless (condition)
   `(unless ,condition (skip)))
